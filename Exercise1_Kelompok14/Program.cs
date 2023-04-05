e72047ce3d71a1c0231fd13253ea32fd979d4e0e
LISA <ASUS@LAPTOP-67G15PD7>
2023-04-05 09:34:45 +0700

    add program connect to database

--- Exercise1_Kelompok14/Program.cs
+++ Exercise1_Kelompok14/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Insert_and_Get_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID :");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password :");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database tujuan :");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk Terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                }
+        }
+        }
+    }
+    
+}
